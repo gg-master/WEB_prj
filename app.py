3e225b97ac15f109da63dcfd75a063d3b5a6e7ba
gg-master <korshun.tolik@yandex.ru>
2021-03-22 15:35:32 +0400

    Написал рабочий шаблон главного окна.

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 import os
 
-from flask import Flask, render_template
-# from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template, request
+from flask_ngrok import run_with_ngrok
 
 from data import db_session
 from data.films import Film
 
 app = Flask(__name__)
-# run_with_ngrok(app)
+run_with_ngrok(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', "POST"])
 def start_page():
     # Инициализация сессии к бд
     # Получение списка фильмов
+    print(request.method)
+    #
+    filter_list = [i for i in request.args]
+    print(filter_list)
+
+    print([i for i in request.form])
+    print([request.form.get(i) for i in request.form])
+    # print(request.args.get(filter_list[0]))
     db_sess = db_session.create_session()
     recommended_films = db_sess.query(Film).filter(Film.rating > 8.0).all()
     films = db_sess.query(Film).all()
-    return render_template('index.html', films=films)
+    filter_dct = {'year': ['2021', '2022', '2023'], 'genre': ['Comedy'], 'rating': ['9.1', '2.1'], 'producer': []}
+
+    return render_template('index.html', films=films, filter=filter_dct)
 
 
 def main():
