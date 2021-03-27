74d3dfe2d5f1078edddc3a122ee818c3363d7c20
gg-master <korshun.tolik@yandex.ru>
2021-03-27 11:21:05 +0400

    Дописал шаблон с расписанием

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask_restful import Api
 
 from api import films_resource, films_api, film_session_resource
 from data import db_session
+from data.film_sessions import FilmSession
 from data.films import Film
 
 app = Flask(__name__)
@@ -42,7 +43,6 @@ def film_description(film_id):
 
 @app.route('/timetable/<int:film_id>', methods=['GET'])
 def timetable(film_id):
-    print()
     btn_day_active = 1
     if request.args:
         btn_day_active = int([i for i in request.args][0])
@@ -50,12 +50,18 @@ def timetable(film_id):
     weekdays = {
         1: 'Вторник', 2: 'Среда', 3: 'Четверг', 4: 'Пятница', 5: 'Суббота',
         6: 'Воскресенье', 0: 'Понедельник'}
-    today = {'today': today, 'weekdays': weekdays,
-             'day_delta': timedelta(days=1)}
+    days_data = {'today': today, 'weekdays': weekdays,
+                 'day_delta': timedelta(days=1)}
     db_sess = db_session.create_session()
     film = db_sess.query(Film).filter(Film.id == film_id).first()
-    return render_template('timetable.html', today=today, film=film,
-                           btn_day_active=btn_day_active)
+    # Узнаем выбранный день, прибавив к текущей дате номер кнопки
+    current_date = today + timedelta(days=btn_day_active - 1)
+    film_sess = list(filter(lambda f: f.start_time.date() == current_date,
+                       db_sess.query(FilmSession).filter(
+                           FilmSession.film_id == film.id).all()))
+    return render_template('timetable.html', today=days_data, film=film,
+                           btn_day_active=btn_day_active,
+                           film_session=film_sess)
 
 
 def main():
@@ -64,8 +70,10 @@ def main():
     api.add_resource(films_resource.FilmResource,
                      '/api/films/<int:film_id>')
     api.add_resource(films_resource.FilmListResource, '/api/films')
-    api.add_resource(film_session_resource.FilmSessionResource, '/api/film_sessions/<int:film_sess_id>')
-    api.add_resource(film_session_resource.FilmSessionListResource, '/api/film_sessions')
+    api.add_resource(film_session_resource.FilmSessionResource,
+                     '/api/film_sessions/<int:film_sess_id>')
+    api.add_resource(film_session_resource.FilmSessionListResource,
+                     '/api/film_sessions')
     # port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=port)
     app.run()
