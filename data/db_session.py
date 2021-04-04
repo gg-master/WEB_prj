b2fc6048dc43741f542d53de031f3f9dd2a02c52
gg-master <korshun.tolik@yandex.ru>
2021-04-04 15:42:59 +0400

    подключение бд

--- data/db_session.py
+++ data/db_session.py
@@ -17,7 +17,9 @@ def global_init(db_file):
     if not db_file or not db_file.strip():
         raise Exception("Необходимо указать файл базы данных.")
     # f'postgresql://postgres:2BoBa1Tolya@127.0.0.1:5432/postgres'
-    conn_str = 'postgres://qpnsqvnwwyyimk:7509fed8004a2c07d70467d00f6dea9607b363dfde31cd56fa6e64072865d48f@ec2-3-91-127-228.compute-1.amazonaws.com:5432/d4ncmqqmq5pi2v'
+    conn_str = 'postgresql://qpnsqvnwwyyimk:7509fed8004a2c07d70467d00f6' \
+               'dea9607b363dfde31cd56fa6e64072865d48f@ec2-3-91-127-228.co' \
+               'mpute-1.amazonaws.com:5432/d4ncmqqmq5pi2v'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
