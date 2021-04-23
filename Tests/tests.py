074b40945f24c6e31ea2addcbc4c20147fc5a76b
KseniiaBondarenko <77286723+KseniiaBondarenko@users.noreply.github.com>
2021-04-23 20:05:14 +0300

    Update tests.py
    Добавила два теста. Тесты ещё будут

--- Tests/tests.py
+++ Tests/tests.py
@@ -18,3 +18,16 @@ def test_film_abort():
     resp = requests.get(
         f'http://{domain}/api/films/199').json()
     assert 'message' in resp and 'not found' in resp['message']
+    
+def test_filter_data_api():
+    arr = ['filter_data']
+    resp = requests.get(
+        f'http://{domain}/api/filter_data').json()
+    assert all(map(lambda x: x in resp, arr))
+    
+def test_filtered_films():
+    arr = ['films']
+    resp = requests.get(
+        f'http://{domain}/api/films/filtered').json()
+    assert all(map(lambda x: x in resp, arr))
+  
