21c4d90098147c7c7be3cc4c242cf27bfddaaa16
gg-master <korshun.tolik@yandex.ru>
2021-05-04 23:40:14 +0400

    Подправил index.html. Теперь если нет новых фильмов, то нету вкладки с новыми фильмами. Изменил тип запуска очищения бд

--- modules/background_clock.py
+++ modules/background_clock.py
@@ -5,7 +5,9 @@ from misc.session_scheduler import delete_film_session_every_week
 sched = BackgroundScheduler({'apscheduler.timezone': 'UTC'})
 
 
-@sched.scheduled_job('interval', hours=24)
+# @sched.scheduled_job('interval', hours=24)
+
+@sched.scheduled_job('cron', day_of_week='tue,fri', hour='15')
 def clear_session_table_background():
     delete_film_session_every_week()
 
--- modules/blocking_clock.py
+++ modules/blocking_clock.py
@@ -3,15 +3,14 @@ from data import db_session
 from apscheduler.schedulers.blocking import BlockingScheduler
 from misc.session_scheduler import delete_film_session_every_week
 
-
 sched = BlockingScheduler({'apscheduler.timezone': 'UTC'})
-
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
 
-@sched.scheduled_job('interval', hours=24)
+# @sched.scheduled_job('interval', hours=24)
+@sched.scheduled_job('cron', day_of_week='tue,fri', hour='15')
 def clear_session_table_blocking():
     logging.info('Log before connect to database')
     if db_session.__factory is None:
